d7eb4642362dcd68c8af5f44176c1dde50a511df
DN <DN@DN.local>
2023-08-08 22:03:56 -0400

    change auth for http call

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ data "http" "example" {
   # Optional request headers
   request_headers = {
     Accept = "multipart/form-data"
-    X-Auth-Email = var.cloudflare_api_email
+    X-Auth-Email = var.cloudflare_api_token
   }
 }
 
