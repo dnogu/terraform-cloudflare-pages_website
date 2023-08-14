113ae1e1a947605030af918bd827f6c1bdc4e475
DN <DN@DN.local>
2023-08-13 22:21:56 -0400

    update data call

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "cloudflare_api_token" {
   description = "The API Token Used for api calls."
 }
 
+variable cloudflare_global_api_key {
+  type        = string
+  description = "API TOKEN For CloudFlare"
+}
+
 variable "custom_domain" {
   type        = string
   default     = "example.com"
