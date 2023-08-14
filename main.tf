6e7662d2d02d033109d0b77ae036805be5dba32e
DN <DN@DN.local>
2023-08-14 16:56:15 -0400

    regorganized variables, added docstrings

--- main.tf
+++ main.tf
@@ -1,23 +1,30 @@
-// This resource configures a custom domain for a Cloudflare Pages project.
+/**
+ * Cloudflare Pages Domain Configuration.
+ *
+ * This resource links a custom domain to a Cloudflare Pages project.
+ * It depends on the Cloudflare Pages project being set up prior to domain association.
+ */
 resource "cloudflare_pages_domain" "pages_domain" {
-  // ID of the Cloudflare account
   account_id   = var.cloudflare_account_id
-  // The project name is derived from the Cloudflare Pages project's source configuration
   project_name = cloudflare_pages_project.source_config.name
-  // Custom domain to be associated with the Cloudflare Pages project
   domain       = var.custom_domain
-  // Ensure the Cloudflare Pages project is created before associating the domain
+
   depends_on = [
     cloudflare_pages_project.source_config
   ]
 }
 
-// This resource sets up a Cloudflare Pages project.
+/**
+ * Cloudflare Pages Project Configuration.
+ *
+ * This resource provides the blueprint for setting up a Cloudflare Pages project.
+ * It includes repository, branch, and build configuration details.
+ */
 resource "cloudflare_pages_project" "source_config" {
   account_id        = var.cloudflare_account_id
   name              = replace(var.custom_domain, ".", "")
   production_branch = "main"
-  // Configuration for the version control system (VCS) source
+
   source {
     type = var.repo_type
     config {
@@ -29,57 +36,52 @@ resource "cloudflare_pages_project" "source_config" {
       production_deployment_enabled = var.pages_deployments_enabled
       preview_deployment_setting    = var.pages_preview_deployment_setting
       preview_branch_includes       = var.preview_branch_includes
-      // Exclude production branch and any other specified branches from previews
       preview_branch_excludes       = distinct(concat(tolist([var.pages_production_branch]), var.preview_branch_excludes))
     }
   }
 
-  // Build and deployment configuration settings
   build_config {
     root_dir        = var.pages_root_dir
     destination_dir = var.pages_destination_dir
   }
 }
 
-// This resource creates a DNS record for the custom domain in Cloudflare, if managed by Cloudflare.
+/**
+ * Cloudflare DNS Record Configuration.
+ *
+ * If managed by Cloudflare, this resource will create a CNAME record for the custom domain.
+ */
 resource "cloudflare_record" "pages_domain" {
   count   = var.cloudflare_managed_dns ? 1 : 0
   zone_id = data.cloudflare_zone.example[count.index].id
-  // Extract the subdomain from the custom domain name
   name    = trimsuffix(var.custom_domain, join("", [".", var.cloudflare_zone_lookup]))
-  // Use the modified domain name as the CNAME value
   value   = "${replace(var.custom_domain, ".", "")}.pages.dev"
   type    = "CNAME"
   ttl     = 3600
 }
 
-// Fetch the Cloudflare zone details, if managed by Cloudflare.
+/**
+ * Cloudflare Zone Data Fetcher.
+ *
+ * If the DNS is managed by Cloudflare, this will fetch the Cloudflare zone details.
+ */
 data "cloudflare_zone" "example" {
   count      = var.cloudflare_managed_dns ? 1 : 0
   account_id = var.cloudflare_account_id
   name       = var.cloudflare_zone_lookup
 }
 
+/**
+ * HTTP Data Source Configuration.
+ *
+ * This is responsible for making a POST request to Cloudflare's API for deployment details.
+ */
 data "http" "example" {
-  url = "https://api.cloudflare.com/client/v4/accounts/${var.cloudflare_account_id}/pages/projects/${cloudflare_pages_project.source_config.name}/deployments"
+  url    = "https://api.cloudflare.com/client/v4/accounts/${var.cloudflare_account_id}/pages/projects/${cloudflare_pages_project.source_config.name}/deployments"
   method = "POST"
-
-  # Optional request headers
   request_headers = {
     "X-Auth-Email" = var.cloudflare_api_email
-    "X-Auth-Key" = var.cloudflare_global_api_key
+    "X-Auth-Key"   = var.cloudflare_global_api_key
     "Content-Type" = "multipart/form-data"
   }
-}
-
-output "response_header" {
-  value   = data.http.example.status_code
-}
-
-output "response_body" {
-  value   = data.http.example.response_body
-}
-
-output "response_headers" {
-  value   = data.http.example.response_headers
 }
--- variables.tf
+++ variables.tf
@@ -1,105 +1,125 @@
+// ------------------------
+// Cloudflare Configuration
+// ------------------------
+
 variable "cloudflare_account_id" {
-  type        = string
   description = "The ID associated with the Cloudflare account."
-}
-
-variable "cloudflare_api_token" {
   type        = string
-  description = "The API Token Used for api calls."
-}
-
-variable "cloudflare_global_api_key" {
-  type        = string
-  description = "API TOKEN For CloudFlare"
-}
-
-variable "custom_domain" {
-  type        = string
-  default     = "example.com"
-  description = "The domain name intended for the website."
-}
-
-variable "repo_type" {
-  type        = string
-  description = "The type of Version Control System (VCS) in use, either 'github' or 'gitlab'."
-}
-
-variable "repo_name" {
-  type        = string
-  description = "The name of the repository housing the website's source code."
-}
-
-variable "pages_production_branch" {
-  type        = string
-  default     = "main"
-  description = "The primary branch used for production in the VCS."
-}
-
-variable "pages_pr_comments_enabled" {
-  type    = bool
-  default = true
-  description = "Flag to indicate if Pull Request comments should be enabled."
-}
-
-variable "pages_deployments_enabled" {
-  type    = bool
-  default = true
-  description = "Flag to determine if deployments are enabled."
-}
-
-variable "pages_preview_deployment_setting" {
-  type    = string
-  default = "custom"
-  description = "Setting to determine how preview deployments are handled."
-}
-
-variable "pages_production_deployment_enabled" {
-  type    = bool
-  default = true
-  description = "Flag to indicate if production deployments are enabled."
-}
-
-variable "preview_branch_includes" {
-  type    = list(string)
-  default = ["preview", "dev"]
-  description = "List of branch names that should be included for previews."
-}
-
-variable "preview_branch_excludes" {
-  type        = list(string)
-  default     = []
-  description = "List of branch names to exclude from previews."
-}
-
-variable "pages_root_dir" {
-  type        = string
-  default     = ""
-  description = "The directory in which the build command is executed."
-}
-
-variable "pages_destination_dir" {
-  type        = string
-  default     = ""
-  description = "The directory within the repository where the HTML content resides."
-}
-
-variable "cloudflare_zone_lookup" {
-  type        = string
-  description = "The DNS zone managed by Cloudflare that's associated with this configuration."
-}
-
-variable "cloudflare_managed_dns" {
-  type        = bool
-  default     = true
-  description = "Boolean to indicate if the DNS Zone is under Cloudflare's management."
-}
-
-variable "repo_owner" {
-  type        = string
-  description = "The individual or organization that owns the specified repository."
 }
 
 variable "cloudflare_api_email" {
+  description = "The email address associated with the Cloudflare API Token."
   type        = string
-  description = "Then email address associated witht the Cloudflare API Token."
+}
+
+variable "cloudflare_api_token" {
+  description = "The API Token used for Cloudflare API calls."
+  type        = string
+}
+
+variable "cloudflare_global_api_key" {
+  description = "Global API key for Cloudflare."
+  type        = string
+}
+
+variable "cloudflare_zone_lookup" {
+  description = "The DNS zone managed by Cloudflare that's associated with this configuration."
+  type        = string
+}
+
+variable "cloudflare_managed_dns" {
+  description = "Boolean to indicate if the DNS Zone is under Cloudflare's management."
+  type        = bool
+  default     = true
+}
+
+// --------------------------
+// Repository Configuration
+// --------------------------
+
+variable "repo_type" {
+  description = "The type of Version Control System (VCS) in use, either 'github' or 'gitlab'."
+  type        = string
+}
+
+variable "repo_name" {
+  description = "The name of the repository housing the website's source code."
+  type        = string
+}
+
+variable "repo_owner" {
+  description = "The individual or organization that owns the specified repository."
+  type        = string
+}
+
+// --------------------------
+// Pages Deployment Settings
+// --------------------------
+
+variable "pages_production_branch" {
+  description = "The primary branch used for production in the VCS."
+  type        = string
+  default     = "main"
+}
+
+variable "pages_pr_comments_enabled" {
+  description = "Flag to indicate if Pull Request comments should be enabled."
+  type        = bool
+  default     = true
+}
+
+variable "pages_deployments_enabled" {
+  description = "Flag to determine if deployments are enabled."
+  type        = bool
+  default     = true
+}
+
+variable "pages_production_deployment_enabled" {
+  description = "Flag to indicate if production deployments are enabled."
+  type        = bool
+  default     = true
+}
+
+variable "pages_preview_deployment_setting" {
+  description = "Setting to determine how preview deployments are handled."
+  type        = string
+  default     = "custom"
+}
+
+variable "preview_branch_includes" {
+  description = "List of branch names that should be included for previews."
+  type        = list(string)
+  default     = ["preview", "dev"]
+}
+
+variable "preview_branch_excludes" {
+  description = "List of branch names to exclude from previews."
+  type        = list(string)
+  default     = []
+}
+
+// --------------------------
+// Build Configuration
+// --------------------------
+
+variable "pages_root_dir" {
+  description = "The directory in which the build command is executed."
+  type        = string
+  default     = ""
+}
+
+variable "pages_destination_dir" {
+  description = "The directory within the repository where the HTML content resides."
+  type        = string
+  default     = ""
+}
+
+// --------------------------
+// Domain Configuration
+// --------------------------
+
+variable "custom_domain" {
+  description = "The domain name intended for the website."
+  type        = string
+  default     = "example.com"
 }
