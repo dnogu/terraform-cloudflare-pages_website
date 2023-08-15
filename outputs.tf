4802aa3f5f0623cdbe5fd0ad428879bb748eef14
DN <DN@DN.local>
2023-08-14 22:11:23 -0400

    remove outputs

--- outputs.tf
+++ outputs.tf
@@ -15,24 +15,6 @@ output "pages_project" {
 }
 
 /**
- * Output for Cloudflare DNS Record Configuration.
- * Only outputted if DNS is managed by Cloudflare.
- */
-output "dns_record" {
-  description = "The DNS record details if the DNS is managed by Cloudflare."
-  value       = var.cloudflare_managed_dns ? cloudflare_record.pages_domain[*].name : "DNS not managed by Cloudflare"
-}
-
-/**
- * Output for Cloudflare Zone Data.
- * Only outputted if DNS is managed by Cloudflare.
- */
-output "cloudflare_zone_data" {
-  description = "Details of the Cloudflare Zone if the DNS is managed by Cloudflare."
-  value       = var.cloudflare_managed_dns ? data.cloudflare_zone.example[*] : "DNS not managed by Cloudflare"
-}
-
-/**
  * Output for HTTP Data Source Configuration.
  */
 output "http_request" {
